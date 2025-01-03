0b1a914410d55ae33d0c4b5346ab0f89e964c0ae
dingjiez <dj35@foxmail.com>
2025-01-03 16:59:26 +0800

    // Contents:    // This is a commit message file

--- get_url.sh
+++ get_url.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# 获取nginx-thrift和media-frontend服务的IP地址和端口号
-NGINX_THRIFT_IP=$(microk8s kubectl get svc nginx-thrift -n social-network -o jsonpath='{.spec.clusterIP}')
-NGINX_THRIFT_PORT=$(microk8s kubectl get svc nginx-thrift -n social-network -o jsonpath='{.spec.ports[0].nodePort}')
+# 获取nginx-thrift服务的IP地址和端口号
+nginx_thrift_info=$(microk8s kubectl get svc nginx-thrift -n social-network -o jsonpath='{.spec.clusterIP}:{.spec.ports[0].nodePort}')
+nginx_thrift_url="http://$nginx_thrift_info"
 
-MEDIA_FRONTEND_IP=$(microk8s kubectl get svc media-frontend -n social-network -o jsonpath='{.spec.clusterIP}')
-MEDIA_FRONTEND_PORT=$(microk8s kubectl get svc media-frontend -n social-network -o jsonpath='{.spec.ports[0].nodePort}')
+# 获取media-frontend服务的IP地址和端口号
+media_frontend_info=$(microk8s kubectl get svc media-frontend -n social-network -o jsonpath='{.spec.clusterIP}:{.spec.ports[0].nodePort}')
+media_frontend_url="http://$media_frontend_info"
 
 # 打印获取到的IP地址和端口号
-echo "nginx-thrift IP: $NGINX_THRIFT_IP, Port: $NGINX_THRIFT_PORT"
-echo "media-frontend IP: $MEDIA_FRONTEND_IP, Port: $MEDIA_FRONTEND_PORT"
+echo "nginx-thrift URL: $nginx_thrift_url"
+echo "media-frontend URL: $media_frontend_url"
 
-# 构建URL
-NGINX_THRIFT_URL="http://$NGINX_THRIFT_IP:$NGINX_THRIFT_PORT"
-MEDIA_FRONTEND_URL="http://$MEDIA_FRONTEND_IP:$MEDIA_FRONTEND_PORT"
-
-# 替换locustfile.py中的变量值
-sed -i -E "s/GLOBAL_NGINX_FRONTEND_URL\s*=\s*'CHANGE_THIS_URL'/GLOBAL_NGINX_FRONTEND_URL = '$NGINX_THRIFT_URL'/" ./locust/locustfile.py
-sed -i -E "s/GLOBAL_MEDIA_FRONTEND_URL\s*=\s*'CHANGE_THIS_URL'/GLOBAL_MEDIA_FRONTEND_URL = '$MEDIA_FRONTEND_URL'/" ./locust/locustfile.py
-
-echo "Updated locustfile.py with new URLs"
+# 替换locustfile.py中的URL
+sed -i "s|GLOBAL_NGINX_FRONTEND_URL = 'CHANGE_THIS_URL'|GLOBAL_NGINX_FRONTEND_URL = '$nginx_thrift_url'|g" locust/locustfile.py
+sed -i "s|GLOBAL_MEDIA_FRONTEND_URL = 'CHANGE_THIS_URL'|GLOBAL_MEDIA_FRONTEND_URL = '$media_frontend_url'|g" locust/locustfile.py
