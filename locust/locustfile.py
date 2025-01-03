7a09e5344ab4ee8f1208dad3d9d76ec1d53e77d9
dingjiez <dj35@foxmail.com>
2025-01-03 17:08:55 +0800

    update locustfile and get_url.sh

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -11,8 +11,8 @@ resource.setrlimit(resource.RLIMIT_NOFILE, (250000, 250000))
 ########################################################################################################################
 # Simulation Configuration
 ########################################################################################################################
-GLOBAL_NGINX_FRONTEND_URL  = 'CHANGE_THIS_URL'
-GLOBAL_MEDIA_FRONTEND_URL  = 'CHANGE_THIS_URL'
+GLOBAL_NGINX_FRONTEND_URL
+GLOBAL_MEDIA_FRONTEND_URL
 
 LOW_DAYS = 3
 HIGH_DAYS = 7
